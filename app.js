aef00f209a267b39c8453a2d6b4bcaf7f98063cb
jitendra <jitendrakumarbunkar85@gmail.com>
2024-03-13 01:11:57 +0530

    vercel setup files added

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+app.get("/", (req, res) => {
+    res.send("HAPPY MOMENT! :) GYWS Backend Server is running...");
+  });
+
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error!";
