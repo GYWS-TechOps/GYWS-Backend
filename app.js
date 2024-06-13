dc9148c3b379104599892744c6605c66191c67d0
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-06-13 13:01:18 +0530

    .

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
 const cors = require("cors");
 const express =require ('express');
 const contactUsRoutes = require("./routes/contactUs");
 const cookieParser = require("cookie-parser");
 
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 
