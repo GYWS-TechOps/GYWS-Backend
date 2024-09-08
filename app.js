f7c16cf5db6d10348019cf886cbf7a40ee6bd152
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-09-08 18:52:55 +0530

    .

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(cors());
 app.use(json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
